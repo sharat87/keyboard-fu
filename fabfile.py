06bba75212e0b127a826c749e0539b3b2c467e32
Shrikant-Sharat <shrikantsharat.k@gmail.com>
2011-05-09 07:35:45 +0530

    A fabfile to automating extension packaging and coffee/less compiling

---
+++ fabfile.py
@@ -0,0 +1,95 @@
+import os, time
+from glob import glob
+from fabric.api import *
+
+def clean():
+
+    if os.path.exists('chrome-app'):
+        local('rm -Rf chrome-app')
+
+def static():
+
+    local('mkdir -p chrome-app')
+
+    local('cp manifest.json chrome-app')
+    local('cp *.html chrome-app')
+    local('cp -R vendor chrome-app')
+    local('cp -R res chrome-app')
+
+def build(f=None):
+
+    local('mkdir -p chrome-app/js')
+    for f in glob('src/*.coffee'):
+        coffee(f, os.path.split(f.replace('.coffee', '.js'))[1])
+
+    local('mkdir -p chrome-app/css')
+    for f in glob('styles/*.less'):
+        lessc(f, os.path.split(f.replace('.less', '.css'))[1])
+
+    local('docco src/api.coffee')
+    local('markdown -f docs/usage.html usage.mkd')
+
+    if os.path.exists('chrome-app/docs'):
+        local('rm -Rf chrome-app/docs')
+
+    local('mv docs chrome-app')
+
+def chrome():
+
+    clean()
+    static()
+    build()
+
+def pack():
+
+    chrome()
+
+    if os.path.exists('key-fu.zip'):
+        local('rm key-fu.zip')
+
+    local('zip -r key-fu.zip chrome-app/*')
+
+def watch():
+
+    globs_to_watch = [
+        'manifest.json',
+        'src/*.coffee',
+        'styles/*.less',
+    ]
+
+    chrome()
+
+    mtimes = {}
+
+    for g in globs_to_watch:
+        for f in glob(g):
+            mtimes[f] = os.stat(f).st_mtime
+
+    while True:
+
+        for f, old_time in mtimes.items():
+
+            # The file may be reported as non-existent for a very brief
+            # period of time sometimes
+            if not os.path.exists(f):
+                continue
+
+            new_time = os.stat(f).st_mtime
+
+            if new_time != old_time:
+                build(f)
+                mtimes[f] = new_time
+
+        time.sleep(.2)
+
+def coffee(infile, outfile):
+    local('coffee -p {infile} > chrome-app/js/{outfile}'.format(
+        infile=infile,
+        outfile=outfile
+        ))
+
+def lessc(infile, outfile):
+    local('lessc {infile} > chrome-app/css/{outfile}'.format(
+        infile=infile,
+        outfile=outfile
+        ))
